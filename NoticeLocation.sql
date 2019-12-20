54a463991586f44656ef903df94d6f6ea4ac6e65
unknown <aqeel.altaf12@gmail.com>
2019-12-20 13:02:51 +0500

    NOtice Location N7 type bug fixed

--- NoticeLocation.sql
+++ NoticeLocation.sql
@@ -2,7 +2,7 @@ ALTER VIEW [dbo].[VW_IMIS_Notice_Location]
 AS 
 select
 dbo.getLetterDate([Assess Year] ,  [Notice Type]  , main.[Billing Entity] ) as [Letter Date],
- [Notice Type], 
+ case when [Notice Type] = 'N7' then 'N6' else [Notice Type] end as [Notice Type], 
  [Current SEQN],
  [Account],
  [Billing Entity],
@@ -11,7 +11,7 @@ dbo.getLetterDate([Assess Year] ,  [Notice Type]  , main.[Billing Entity] ) as [
 (
   select
 base.Test as [Letter Date],
-case when base.[NoticeType] = 'N7' then 'N6' else base.[NoticeType] end as [Notice Type],
+base.[NoticeType] as [Notice Type],
 base.SEQN   as [Current SEQN],
 base.Id as [Account],
 IMIS_Service.dbo.fn_TransParentID(base.Id, base.ASSESS_YEAR)  as [Billing Entity],
