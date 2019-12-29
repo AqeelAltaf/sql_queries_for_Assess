f214a8baa44b78549541b45fe097f354fca6a971
unknown <aqeel.altaf12@gmail.com>
2019-12-29 09:57:21 +0500

    Notice Location changes added

---
+++ notice_location_changes.sql
@@ -0,0 +1,35 @@
+CREATE VIEW [dbo].[VW_IMIS_Notice_Location_Changes]
+AS 
+select
+dbo.getLetterDate([Assess Year] ,  [Notice Type]  , main.[Billing Entity] ) as [Letter Date],
+ case when [Notice Type] = 'N7' then 'N6' else [Notice Type] end as [Notice Type], 
+ [Current SEQN],
+ [Account],
+ [Billing Entity],
+  [Assess Year]
+  from 
+(
+  select
+base.Test as [Letter Date],
+base.[NoticeType] as [Notice Type],
+base.SEQN   as [Current SEQN],
+base.[IMIS Account Number] as [Account],
+IMIS_Service.dbo.fn_TransParentID(base.[IMIS Account Number], base.ASSESS_YEAR)  as [Billing Entity],
+base.ASSESS_YEAR as [Assess Year],
+IMIS_name.STATUS as [Status Flag]
+
+ from 
+ ( 
+     select *
+--  id,SEQN,Notice_Type,Test
+FROM BOOMI.dbo.vIMIS_Assess_Notice_CHANGES 
+unpivot
+(
+  Test
+  for  NoticeType in (AQ1,AQ2,AQ3,N1,N2,N3,N4,N5,N6,N7,B1,B2,B3,B4,B5,A1,A2,A3)
+) as NoticeType  ) base
+ LEFT JOIN IMIS.dbo.Name IMIS_name on base.[IMIS Account Number] =  IMIS_name.ID ) main where main.[Status Flag] !='D' 
+
+GO
+
+
