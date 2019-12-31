ea3f1b29eefad3154664bde3de30ebd260eb0f4d
unknown <aqeel.altaf12@gmail.com>
2019-12-31 20:27:05 +0500

    --

--- fiscalmonth.sql
+++ fiscalmonth.sql
@@ -65,3 +65,40 @@ where acc.CATEGORY_TYPE__C  = 'Parent'
 or (acc.BILL_TO_PARENT__C is Null and acc.CATEGORY_TYPE__C  = 'Exempt') 
 and assess.ASSESS_YEAR in ('2017/18','2018/19','2019/20')) _ group by a 
 
+
+-- function to get Fiscal Month for the bill by the most count of fiscal month from their children
+ALTER FUNCTION dbo.getFiscalMonthforAccount(@Parent varchar(255))  
+RETURNS VARCHAR(255) AS
+BEGIN
+   RETURN 
+	(
+	
+	select top 1 
+case when count(*) over(partition by  [count of BIll Cycles] ) > 1 then 'tie'  else BILLING_CYCLE__C  end from (
+select top 2 BILLING_CYCLE__C, count(*) as [count of BIll Cycles] from BOOMI_DEV.dbo.PRODAccounts  where  BILL_TO_PARENT__C = @Parent 
+ group by BILLING_CYCLE__C  ) base
+ORDER BY [count of BIll Cycles] DESC  ) 
+End
+go
+
+ -- query to get bils/parents from account whose locations/childs have different Billing Cycle
+ select * from ( select ID,CATEGORY_TYPE__C, (select count(distinct BILLING_CYCLE__C) from BOOMI_DEV.dbo.PRODAccounts  where  BILL_TO_PARENT__C =base.ID)  as [dist fm count] from BOOMI_DEV.dbo.PRODAccounts  base) base  where CATEGORY_TYPE__C = 'Parent'  and  [dist fm count] > 1     
+ 
+ -- query to get tie cases  in Account Fiscal MOnth 
+ select  acc.BILL_TO_PARENT__C , 
+acc.BILLING_CYCLE__C,
+ acc.ID 
+from 
+(
+select * from  (
+	select ID ,
+	--dbo.getFiscalMonthforAccount(ID) as [Fiscal Month],
+	CATEGORY_TYPE__C, 
+	(select count(distinct BILLING_CYCLE__C) from BOOMI_DEV.dbo.PRODAccounts  where  BILL_TO_PARENT__C =base.ID)  as [dist fm count]
+	 from 
+	 (select * from  BOOMI_DEV.dbo.PRODAccounts   where CATEGORY_TYPE__C = 'Parent' ) base   ) _     
+	 where [dist fm count] > 1  ) main 
+	 LEFT JOIN BOOMI_DEV.dbo.PRODAccounts acc on acc.BILL_TO_PARENT__C = main.ID  
+	 where dbo.getFiscalMonthforAccount(main.ID) = 'tie'
+	 ORDER BY acc.BILL_TO_PARENT__C, acc.ID , acc.BILLING_CYCLE__C 
+
